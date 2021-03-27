4871bd70ee32daad6785ecbf1a0226fc23b4e487
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-03-27 17:12:39 +0530

    Update Alert.jsx

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -21,7 +21,7 @@ function Alert() {
             <div className="cod">
                 <Iframe
                     url="https://carbon.now.sh/embed?bg=rgba%2852%2C73%2C85%2C1%29&t=seti&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=27px&ph=22px&ln=false&fl=1&fm=Hack&fs=13px&lh=118%25&si=false&es=2x&wm=false&code=%253Cdiv%2520class%2520%253D%2520%2522alert-s%2522%2520%253E%2520This%2520is%2520a%2520success%2520alert.%2520%253C%252Fdiv%253E%250A%253Cdiv%2520class%2520%253D%2520%2522alert-d%2522%2520%253E%2520This%2520is%2520a%2520danger%2520alert.%2520%2520%253C%252Fdiv%253E%250A%253Cdiv%2520class%2520%253D%2520%2522alert-w%2522%2520%253E%2520This%2520is%2520a%2520warning%2520alert.%2520%253C%252Fdiv%253E%250A%253Cdiv%2520class%2520%253D%2520%2522alert-i%2522%2520%253E%2520This%2520is%2520a%2520info%2520alert.%2520%2520%2520%2520%253C%252Fdiv%253E%250A%253Cdiv%2520class%2520%253D%2520%2522alert-p%2522%2520%253E%2520This%2520is%2520a%2520primary%2520alert.%2520%253C%252Fdiv%253E%250A%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520%2520"
-                    width=" 100%" height="212px" border="0" transform="scale(1)" overflow="scroll" frameborder="0"
+                    width=" 100%" height="212px" border="0" transform="scale(1)" overflow="scroll" frameBorder="0"
                 />
 
             </div>
