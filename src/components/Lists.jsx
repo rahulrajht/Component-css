a1899e939815f78e0301c309578e83c3c4e4e811
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-03-27 17:21:37 +0530

    Update Lists.jsx

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -23,7 +23,7 @@ function Lists() {
             <div className="cod">
                 <Iframe
                     url="https://carbon.now.sh/embed?bg=rgba%2852%2C73%2C85%2C1%29&t=seti&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=27px&ph=22px&ln=false&fl=1&fm=Hack&fs=14px&lh=144%25&si=false&es=2x&wm=false&code=%252F*%2520This%2520is%2520a%2520Simple%2520Unordered%2520List%2520*%252F%250A%253Cul%2520class%2520%253D%2520%2522ul-lists%2522%2520%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25201%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25202%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25203%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25204%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25205%2520%253C%252Fli%253E%250A%253C%252Ful%253E%250A"
-                    width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden"
+                    width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden" frameBorder="0"
                     sandbox="allow-scripts allow-same-origin">
                 </Iframe>
             </div>
@@ -41,7 +41,7 @@ function Lists() {
             <div className="cod">
                 <Iframe
                     url="https://carbon.now.sh/embed?bg=rgba%2852%2C73%2C85%2C1%29&t=seti&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=27px&ph=22px&ln=false&fl=1&fm=Hack&fs=14px&lh=144%25&si=false&es=2x&wm=false&code=%252F*%2520This%2520is%2520a%2520%2520Unordered%2520List%2520Box%2520*%252F%250A%253Cul%2520class%2520%253D%2520%2522box-list%2522%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25201%2520%253Cinput%2520type%2520%253D%2520%2522checkbox%2522%2520%252F%253E%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25202%2520%253Cinput%2520type%2520%253D%2520%2522checkbox%2522%2520%252F%253E%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25203%2520%253Cinput%2520type%2520%253D%2520%2522checkbox%2522%2520%252F%253E%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25204%2520%253Cinput%2520type%2520%253D%2520%2522checkbox%2522%2520%252F%253E%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25205%2520%253Cinput%2520type%2520%253D%2520%2522checkbox%2522%2520%252F%253E%2520%253C%252Fli%253E%250A%253C%252Ful%253E%250A"
-                    width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden"
+                    width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden" frameBorder="0"
                     sandbox="allow-scripts allow-same-origin">
                 </Iframe>
             </div>
@@ -58,11 +58,11 @@ function Lists() {
 
             </div>
             <div className="cod">
-            <Iframe
-  url="https://carbon.now.sh/embed?bg=rgba%2852%2C73%2C85%2C1%29&t=seti&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=27px&ph=22px&ln=false&fl=1&fm=Hack&fs=13.5px&lh=146%25&si=false&es=2x&wm=false&code=%252F*%2520This%2520is%2520a%2520simple%2520stacked%2520list%2520with%2520hover%2520effect.%2520*%252F%250A%253Cul%2520class%2520%253D%2520%2522stack-list%2522%2520%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25201%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25202%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25203%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25204%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25205%2520%253C%252Fli%253E%250A%253C%252Ful%253E%250A"
-  width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden"
-  sandbox="allow-scripts allow-same-origin">
-</Iframe>
+                <Iframe
+                    url="https://carbon.now.sh/embed?bg=rgba%2852%2C73%2C85%2C1%29&t=seti&wt=none&l=auto&ds=false&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=27px&ph=22px&ln=false&fl=1&fm=Hack&fs=13.5px&lh=146%25&si=false&es=2x&wm=false&code=%252F*%2520This%2520is%2520a%2520simple%2520stacked%2520list%2520with%2520hover%2520effect.%2520*%252F%250A%253Cul%2520class%2520%253D%2520%2522stack-list%2522%2520%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25201%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25202%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25203%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25204%2520%253C%252Fli%253E%250A%2520%2520%253Cli%253E%2520This%2520is%2520list%2520Item%25205%2520%253C%252Fli%253E%250A%253C%252Ful%253E%250A"
+                    width=" 100%" height="310px" border="0" transform="scale(1)" overflow="hidden" frameBorder="0"
+                    sandbox="allow-scripts allow-same-origin">
+                </Iframe>
 
             </div>
 
